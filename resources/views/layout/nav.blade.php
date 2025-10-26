1f086cdcb0350d2bea2c98be590c9377e7745dc3
Boris Nacev <bnachev77@gmail.com>
2025-10-26 02:01:35 +0100

    landing page

---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,14 @@
+<div class="d-flex align-items-center justify-content-between mb-4  ">
+    <div class="d-flex gap-3 align-items-center">
+        <div class="mb-0 fw-bold fs-5">Progressor</div>
+        <div class="d-flex align-items-center gap-4">
+            <a href="" class="btn">Home</a>
+            <a href="" class="btn">Discover</a>
+            <a href="" class="btn">Collections </a>
+        </div>
+    </div>
+    <div class="d-flex align-items-center gap-3">
+        <div class="btn btn-primary">Login</div>
+        <div class="btn btn-secondary">Sign up</div>
+    </div>
+</div>
